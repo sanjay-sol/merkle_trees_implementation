916ec8d0116a251b361c671a66e5122ac909d98c
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-07-09 17:39:39 +0530

    TS: Types declared

--- typescript/src/merkle_tree.ts
+++ typescript/src/merkle_tree.ts
@@ -1,1 +1,28 @@
-console.log("asd")
+
+import { keccak256 } from 'ethereumjs-util';
+
+type Tree = {
+
+}
+
+type Root = {
+
+}
+
+type TreeNode = {
+
+}
+type Proof = typeof keccak256[] | typeof keccak256;
+
+
+class MerkleTree  {
+    constructor() { }
+
+    getRoot(): Root {
+        return 
+    }
+
+  verify(proof: Proof, root: Root, node: TreeNode): boolean {
+    return true || false;
+  }
+}
